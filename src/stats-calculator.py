d133117e60f0014612ad8f10b630387fe4eeb26b
Karan Ramani <kjr27@njit.edu>
2020-11-05 16:00:28 -0500

    Feature: Random number generator in a list without a SEED

--- src/stats-calculator.py
+++ src/stats-calculator.py
@@ -24,3 +24,21 @@ print(round(random.uniform(1,30),4))
 print("\n")
 print("------------------------------------------------------")
 print("\n")
+
+# Random number list without a seed
+randomA = []
+randomB = []
+for i in range(0,10):
+    x = random.randrange(100,200)
+    y = round(random.uniform(1,10),4)
+    randomA.append(x)
+    randomB.append(y)
+
+print("Random Numbers list Without SEED: \n")
+print("Integer Numbers: ")
+print(randomA)
+print("\nFloat Numbers: ")
+print(randomB)
+print("\n")
+print("------------------------------------------------------")
+print("\n")
