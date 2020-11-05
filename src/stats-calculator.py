2fe966dec5d2230d6aa46b412122fd038360a6a8
Karan Ramani <kjr27@njit.edu>
2020-11-05 15:44:25 -0500

    Feature: stats-calculator.py single random number generator

---
+++ src/stats-calculator.py
@@ -0,0 +1,15 @@
+import random
+
+print("\n")
+print("\n")
+
+# Random number without a seed (SINGLE)
+print ("Generating random number integer (without a seed):")
+print(random.randrange(100,200))
+
+print ("Generating random number decimal (without a seed):")
+print(round(random.uniform(1,10),4))
+
+print("\n")
+print("------------------------------------------------------")
+print("\n")
