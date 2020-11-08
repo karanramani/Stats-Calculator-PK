505dd26bc72c9c2ee8f43d6b7b8f79c5ce3ebabd
priyesha97513 <ppb7@njit.edu>
2020-11-08 08:24:02 -0500

    Feature: Median func and class included

--- src/Calculator.py
+++ src/Calculator.py
@@ -9,11 +9,17 @@ def mean_main(a):
     x = np.mean(a)
     return x
 
-
+def median_main(a):
+    x = np.median(a)
+    return x
 
 class Calculator:
     result = 0
 
     def mean1(a):
         result = mean_main(a)
+        return result
+
+    def median1(a):
+        result = median_main(a)
         return result
